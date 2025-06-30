a7e83bc7af855a8f6a65e6aeb313633cb876fff0
shrutikumari03 <shrutikumari8532@gmail.com>
2025-06-30 22:14:08 +0530

    added my task file

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleTheme() {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  const newTheme = currentTheme === "light" ? "dark" : "light";
+  document.documentElement.setAttribute("data-theme", newTheme);
+}
